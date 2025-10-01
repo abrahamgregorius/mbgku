8b23e5e4c71706a9bc2eaaf8a0a7987a79261d05
Jefferson <jefferson.swanto@gmail.com>
2025-10-01 14:08:14 +0700

    feat: add dashboard card ui

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,16 +1,49 @@
 import React from "react";
 import DashboardLayout from "../layout/DashboardLayout";
 import AppBarChart from "../ui/common/AppBarChart";
+import CardDashboard from "../ui/common/CardDashboard";
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
       <div className="flex flex-col gap-4 p-4">
         <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 ">
-          <div className="bg-green-200 p-4 rounded-lg"></div>
-          <div className="bg-green-200 p-4 rounded-lg"></div>
-          <div className="bg-green-200 p-4 rounded-lg"></div>
-          <div className="bg-green-200 p-4 rounded-lg"></div>
+          <div className=" p-4 rounded-lg">
+            <CardDashboard
+              header={"Total Revenue"}
+              title={"$1,250.00"}
+              stat={"+12.5%"}
+              description={"Trending up this month"}
+              footer={"Visitors for the last 6 months"}
+            />
+          </div>
+          <div className=" p-4 rounded-lg">
+            <CardDashboard
+              header={"Total Revenue"}
+              title={"$1,250.00"}
+              stat={"+12.5%"}
+              description={"Trending up this month"}
+              footer={"Visitors for the last 6 months"}
+            />
+          </div>
+          <div className=" p-4 rounded-lg">
+            <CardDashboard
+              header={"Total Revenue"}
+              title={"$1,250.00"}
+              stat={"+12.5%"}
+              description={"Trending up this month"}
+              footer={"Visitors for the last 6 months"}
+            />
+          </div>
+          <div className=" p-4 rounded-lg">
+            <CardDashboard
+              header={"Total Revenue"}
+              title={"$1,250.00"}
+              stat={"+12.5%"}
+              description={"Trending up this month"}
+              footer={"Visitors for the last 6 months"}
+            />
+          </div>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
           <div className="p-4 rounded-lg">
